c79fc8d14501a8284b10b6eeb46a528614baa2a5
makuogideon <umunnakwemakuochukwugideon@gmail.com>
2023-07-14 20:58:56 +0000

    task 3 and 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main -this is the main function
+ *
+ * Return: return to 0 when successful
+ */
 
+int main(void)
+{
+	puts("\"programming is like building multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: return to 0 if successful
+ */
+
+int main(void)
+{
+	printf("with a proper grammer, but the outcome is a piece of art,\n")
+	return (0);
+}
