6b7872c2a78ec0f64d718824bdf7291944fdcb76
makuogideon <umunnakwemakuochukwugideon@gmail.com>
2023-07-15 18:45:29 +0000

    taks 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char MyStrl [] = "\"Programming is like building a multilingual puzzle"
+	char MyStrl [] = "\"Programming is like building a multilingual puzzle";
 	
 	puts(MyStril);
 	
