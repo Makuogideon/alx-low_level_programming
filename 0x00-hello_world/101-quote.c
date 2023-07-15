e6109e81f0863763c78f48fdc0fc98a92b7acec3
makuogideon <umunnakwemakuochukwugideon@gmail.com>
2023-07-15 21:39:20 +0000

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a C file that print with error when return
+ * Return: return to (1) when done
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
+	return (1);
+
+}
